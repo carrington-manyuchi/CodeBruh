751e9f8e5eae6fe7f5e01beaaeb6623b7e24ec64
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-01-10 21:37:41 +0200

    Model

---
+++ CodeBruh/Strings.swift
@@ -0,0 +1,17 @@
+//
+//  Strings.swift
+//  CodeBruh
+//
+//  Created by Manyuchi, Carrington C on 2025/01/10.
+//
+
+import Foundation
+
+
+/// Crypto App
+/// Basic project setup
+/// Programmatic setup
+/// Generic API CAlls later
+/// How to fetch and display data from  a remote JSON API
+/// search function
+/// 4ff192bd-42e1-40be-8250-bce0f832d8ea
---
+++ CodeBruh/Supporting/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  CodeBruh
+//
+//  Created by Manyuchi, Carrington C on 2025/01/10.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    //MARK: - API
+    static let scheme = "https"
+    static let baseURL = "https://pro.coinmarketcap.com/"
+    static let port: Int? = nil
+    static let apiKey = "4ff192bd-42e1-40be-8250-bce0f832d8ea"
+}
